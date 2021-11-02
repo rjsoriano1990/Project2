207e24cbbb97aa23882a4853774c090923f0c6e3
That Guy <sochenduszko@yahoo.com>
2021-11-01 17:50:50 -0700

    schema and notebooks added

---
+++ schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE covid_case_death (
+id SERIAL PRIMARY KEY,
+submission_date DATE,
+state VARCHAR (5),
+tot_cases INT,
+new_case INT,
+tot_death INT,
+new_death INT
+);
+
+CREATE TABLE covid_vac (
+id SERIAL PRIMARY KEY,
+submission_date DATE,
+state VARCHAR (5),
+distributed INT,
+distributed_janssen INT,
+distributed_moderna INT,
+distributed_pfizer INT,
+dist_per_100K INT,
+administered INT,
+administered_janssen INT,
+administered_moderna INT,
+administered_pfizer INT,
+recip_administered INT,
+administered_dose1_recip INT,
+administered_dose1_pop_pct INT
+);
+
+CREATE TABLE us_unemployment (
+id SERIAL PRIMARY KEY,
+submission_date DATE,
+state VARCHAR (5),
+unemployment_rate FLOAT
+);
+
